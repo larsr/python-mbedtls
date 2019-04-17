b1a12d31b88eca72eaa00dd25d43c6760b943d0b
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2019-04-17 11:42:17 +0200

    change cython language level to 3str

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ def extensions(coverage=False):
                 if coverage
                 else [],
             )
-            extension.cython_directives = {"language_level": 3}
+            extension.cython_directives = {"language_level": "3str"}
             if coverage:
                 extension.cython_directives["linetrace"] = True
             yield extension
